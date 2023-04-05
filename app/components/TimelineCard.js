560064065f79b61dd54d4f93f06785bd4b1f9967
Jordan C <jaycedam@gmail.com>
2023-04-05 00:11:31 -0400

    added type for jobs

--- app/components/TimelineCard.js
+++ app/components/TimelineCard.js
@@ -2,6 +2,7 @@ export default function TimelineCard(props) {
   // use with a ul wrapper
   return (
     <li className="theme-card">
+      <p className="font-light text-sm">{props.type}</p>
       <div className="flex items-center gap-2">
         <h2 className="font-extrabold text-lg">{props.title}</h2>
         <h3 className="text-sm font-light">{props.date}</h3>
