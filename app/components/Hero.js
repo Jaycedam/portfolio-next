178336d96381be11691be68518b1d9e3f8846eb4
Jordan C <jaycedam@gmail.com>
2023-04-12 22:32:17 -0400

    hero adjustments, more tailwind components

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -1,105 +1,74 @@
-import { MdEmail } from "react-icons/md";
-import Image from "next/image";
-import image from "../assets/profile.jpg";
-
 import {
   AiFillGithub,
   AiFillDribbbleCircle,
   AiFillLinkedin,
 } from "react-icons/ai";
+import { FaCode } from "react-icons/fa";
 
 export default function Hero() {
+  // bg is inside "after:"
   return (
     <section
-      className="container px-4 mx-auto pt-32 mb-16 grid gap-4 h-[70vh] 
-        portrait:h-[50vh]
-        md:h-[80vh]
-        md:grid-cols-2 grid-flow-dense"
-    >
-      {/* image section  */}
-      <div className="md:col-start-2 relative">
-        {/* <Image src={image} className="w-full h-full object-cover rounded-2xl" /> */}
+      className="relative isolate
+      w-full h-screen max-h-[1500px]
 
-        {/* animated blurred blobs background */}
-        <div
-          className="absolute top-0 -left-4 w-96 h-96 
-            bg-purple-500 rounded-full mix-blend-multiply filter blur-xl opacity-30 
-            animate-blob
-            dark:mix-blend-hard-light"
-        ></div>
-        <div
-          className="absolute top-0 -right-4 w-96 h-96 
-            bg-cyan-300 rounded-full mix-blend-multiply filter blur-xl opacity-30 
-            animate-blob animation-delay-2000
-            dark:mix-blend-hard-light"
-        ></div>
-        <div
-          className="absolute -bottom-8 left-20 w-96 h-96 
-            bg-pink-400 rounded-full mix-blend-multiply filter blur-xl opacity-30 
-            animate-blob animation-delay-4000
-            dark:mix-blend-hard-light"
-        ></div>
+      after:content-[''] after:absolute 
+      after:w-screen after:h-full 
+      after:bg-[url('/bg.png')] after:bg-cover after:bg-center after:bg-fixed
+      after:rounded-b-2xl after:-z-10
+      
+      container container-fix
+      flex-center flex-col gap-8"
+    >
+      <header className="grid gap-2 text-white">
+        <h1
+          className="font-black text-5xl 
+            drop-shadow-xl
+              md:text-7xl
+              lg:text-8xl"
+        >
+          Software
+          <br />
+          Dev.
+        </h1>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, ipsum.
+        </p>
+      </header>
+
+      <a href="#software" className="btn-hero">
+        Ver proyectos
+        <span>
+          <FaCode />
+        </span>
+      </a>
+
+      {/* social icons  */}
+      <div
+        className="absolute top-10 text-3xl flex gap-4 text-zinc-50
+          md:left-0 md:top-auto md:flex-col"
+      >
+        <a href="#" aria-label="GitHub Link">
+          <AiFillGithub />
+        </a>
+
+        <a href="#" aria-label="Dribbble Link">
+          <AiFillDribbbleCircle />
+        </a>
+
+        <a href="#" aria-label="Linkedin Link">
+          <AiFillLinkedin />
+        </a>
       </div>
 
-      {/* text section */}
-      <div className="flex flex-col justify-center gap-8">
-        {/* text  */}
-        <header className="grid gap-2">
-          {/* social icons  */}
-          <ul className="flex gap-4 w-full text-xl text-zinc-500">
-            <li>
-              <a
-                href="https://github.com/Jaycedam"
-                target="_blank"
-                className="flex gap-2 items-center text-center"
-              >
-                <AiFillGithub />
-              </a>
-            </li>
-
-            <li>
-              <a
-                href="https://dribbble.com/jaycedam"
-                target="_blank"
-                className="flex gap-2 items-center text-center"
-              >
-                <AiFillDribbbleCircle />
-              </a>
-            </li>
-
-            <li>
-              <a
-                href="https://www.linkedin.com/in/jaycedam/"
-                target="_blank"
-                className="flex gap-2 items-center text-center"
-              >
-                <AiFillLinkedin />
-              </a>
-            </li>
-          </ul>
-
-          <h1
-            className="font-black text-5xl md:text-6xl
-              lg:text-7xl"
-          >
-            Developer
-            <span className="text-zinc-500"> ./</span>
-            <br />
-            Motion
-          </h1>
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis,
-            ipsum.
-          </p>
-        </header>
-
-        {/* call to action  */}
-        <button>
-          <a href="#software" className="btn-primary">
-            Ver proyectos
-          </a>
-        </button>
+      {/* decoration right  */}
+      <div
+        className="-right-12 absolute rotate-90 text-zinc-50 font-thin text-3xl tracking-wider
+            hidden
+            md:block"
+      >
+        hello, world.
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#EA047E",
+        primary: "#E11299",
         secondary: "#00F5FF",
         // ...
       },
