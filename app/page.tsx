7998c2c1ab0c60af941417602fa6b6255162352c
Jordan C <jaycedam@gmail.com>
2023-04-20 00:50:13 -0400

    rm old imported font

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import { Inter } from "next/font/google";
-const inter = Inter({ subsets: ["latin"] });
 import Hero from "./components/Hero";
 import Software from "./components/Software";
 import Motion from "./components/Motion";
