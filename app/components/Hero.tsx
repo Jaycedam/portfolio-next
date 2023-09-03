6ff53d37882423fbde5928a65644236b93402bc3
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2023-09-03 08:20:53 -0300

    change hero layout, remove svg animation

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,7 +1,6 @@
 import Button from "./Button";
-import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 import { FaCode } from "react-icons/fa";
-import LaptopAnimation from "./LaptopAnimation";
+import LaptopSVG from "./LaptopSVG";
 import ThemeToggle from "./ThemeToggle";
 
 export default function Hero() {
@@ -10,39 +9,31 @@ export default function Hero() {
       className="relative isolate mb-0 overflow-x-hidden
          md:mt-14"
     >
-      <div
-        className="container flex flex-col justify-center gap-8 py-20 md:flex-row-reverse md:items-center md:py-36
-          [&>div]:w-full md:[&>div]:w-2/4"
-      >
+      <div className="container my-16 flex flex-col items-center justify-center gap-4 text-center">
         {/* hero graphic  */}
-        <div className="[&>*]:h-full [&>*]:w-full [&>*]:object-contain">
-          <LaptopAnimation />
+        <div className="max-w-lg [&>*]:h-full [&>*]:w-full [&>*]:object-contain">
+          <LaptopSVG />
         </div>
 
-        <div
-          className="flex flex-col items-center gap-4 text-center
-            md:items-start md:text-start"
-        >
-          <ThemeToggle />
+        <ThemeToggle />
 
-          <header className="mt-4 max-w-xl space-y-2">
-            <h1 className="text-3xl font-bold md:text-4xl">
-              Hola, soy
-              <br />
-              <strong className="gradient animate-gradient bg-clip-text text-6xl font-black text-transparent md:text-8xl">
-                Jordan Cortés.
-              </strong>
-            </h1>
+        <header className="mt-4 space-y-2">
+          <h1 className="text-3xl font-bold md:text-4xl">
+            Hola, soy
+            <br />
+            <strong className="gradient animate-gradient bg-clip-text text-6xl font-black text-transparent md:text-8xl">
+              Jordan Cortés
+            </strong>
+          </h1>
 
-            <p>
-              Desarrollador de Software.
-              <br />
-              Ocacionalmente trabajo en Motion Graphics.
-            </p>
-          </header>
+          <p>
+            Desarrollador de Software.
+            <br />
+            Ocacionalmente trabajo en Motion Graphics.
+          </p>
+        </header>
 
-          <Button link="#projects" text="Ver proyectos" icon={<FaCode />} />
-        </div>
+        <Button link="#projects" text="Ver proyectos" icon={<FaCode />} />
       </div>
     </section>
   );
