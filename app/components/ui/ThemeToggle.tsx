2f7164cf4429f255dac5864da2dd2514ea6ee500
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2023-12-30 14:20:53 -0300

    shadcn ui components added (themetoggle, btn, dropdown)

--- app/components/ui/ThemeToggle.tsx
+++ app/components/ui/ThemeToggle.tsx
@@ -1,81 +1,40 @@
 "use client";
+
+import * as React from "react";
 import { BsMoonStars, BsSun } from "react-icons/bs";
-import { useState, useEffect, Fragment } from "react";
 import { useTheme } from "next-themes";
-import { HiComputerDesktop } from "react-icons/hi2";
 
-const ThemeToggle = () => {
-  const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
-  // useEffect only runs on the client, so now we can safely show the UI
-  // Prevents desynct UI server - client
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  // Returns placeholder if client hasn't mounted the component yet
-  // this is to prevent desync from server and client UI due to next-themes pckg
-  // Hidden from screenreaders
-  if (!mounted) {
-    return (
-      <div
-        aria-hidden
-        className="flex h-10 items-center rounded-full px-1 outline outline-1 outline-zinc-50/30"
-      >
-        <button className="aspect-square h-9 rounded-full p-2">
-          <BsSun className="h-full w-auto" />
-        </button>
-        <button className="aspect-square h-9 rounded-full p-2">
-          <HiComputerDesktop className="h-full w-auto" />
-        </button>
-        <button className="aspect-square h-9 rounded-full p-2">
-          <BsMoonStars className="h-full w-auto" />
-        </button>
-      </div>
-    );
-  }
+export function ThemeToggle() {
+  const { setTheme } = useTheme();
 
   return (
-    <div
-      role="radiogroup"
-      className="flex h-10 items-center rounded-full px-1 outline outline-1 outline-zinc-500/60"
-    >
-      <button
-        role="radio"
-        aria-label="Switch to light theme"
-        aria-checked={theme === "light" ? "true" : "false"}
-        className={`aspect-square h-9 rounded-full p-2 ${
-          theme === "light" ? "bg-zinc-300 dark:bg-zinc-600" : ""
-        }`}
-        onClick={() => setTheme("light")}
-      >
-        <BsSun className="h-full w-auto" />
-      </button>
-      <button
-        role="radio"
-        aria-label="Switch to system theme"
-        aria-checked={theme === "system" ? "true" : "false"}
-        className={`aspect-square h-9 rounded-full p-2 ${
-          theme === "system" ? "bg-zinc-600" : ""
-        }`}
-        onClick={() => setTheme("system")}
-      >
-        <HiComputerDesktop className="h-full w-auto" />
-      </button>
-      <button
-        role="radio"
-        aria-label="Switch to dark theme"
-        aria-checked={theme === "dark" ? "true" : "false"}
-        className={`aspect-square h-9 rounded-full p-2 ${
-          theme === "dark" ? "bg-zinc-600" : ""
-        }`}
-        onClick={() => setTheme("dark")}
-      >
-        <BsMoonStars className="h-full w-auto" />
-      </button>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <BsSun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <BsMoonStars className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
-};
-
-export default ThemeToggle;
+}
