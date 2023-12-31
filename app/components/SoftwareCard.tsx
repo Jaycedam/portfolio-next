22273d6c7f7a08020295fe3e28490a2ad5a27f18
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2023-12-31 17:04:00 -0300

    removed empty div, change gap of skeleton

--- app/components/SoftwareCard.tsx
+++ app/components/SoftwareCard.tsx
@@ -5,36 +5,32 @@ import Link from "next/link";
 
 export default function SoftwareCard(props: Software) {
   return (
-    <>
-      {/* image with link to project */}
-      <Link href={`/software/${props.id}`}>
-        <div
-          className="group relative isolate aspect-square overflow-hidden rounded-md
+    <Link href={`/software/${props.id}`}>
+      <div
+        className="group relative isolate aspect-square overflow-hidden rounded-md
              transition-all duration-500"
-        >
-          {/* overlay  */}
-          <div
-            className="pointer-events-none absolute bottom-0 left-0 z-20 grid
+      >
+        {/* overlay  */}
+        <div
+          className="pointer-events-none absolute bottom-0 left-0 z-20 grid
               w-full items-center justify-center bg-gradient-to-t
               from-zinc-950/80 pb-8 
               pt-12 text-lg 
               text-zinc-50"
-          >
-            {props.name} - {ProjectType[props.areaId]}
-          </div>
-
-          <Image
-            src={props.imageUrl}
-            alt="project-image"
-            quality={100}
-            fill
-            sizes="(max-width: 768px) 100vw, 50vw"
-            className="h-full w-full object-cover 
+        >
+          {props.name} - {ProjectType[props.areaId]}
+        </div>
+        <Image
+          src={props.imageUrl}
+          alt="project-image"
+          quality={100}
+          fill
+          sizes="(max-width: 768px) 100vw, 50vw"
+          className="h-full w-full object-cover 
             transition-all duration-500 group-hover:scale-110 group-active:scale-100
             "
-          />
-        </div>
-      </Link>
-    </>
+        />
+      </div>
+    </Link>
   );
 }
--- app/software/loading.tsx
+++ app/software/loading.tsx
@@ -2,8 +2,8 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div className="mx-auto mt-24 grid max-w-4xl gap-4">
-      <Skeleton className="h-16 w-2/4" />
+    <div className="mx-auto mt-24 grid max-w-4xl gap-6">
+      <Skeleton className="h-12 w-2/4" />
       <Skeleton className="h-4 w-3/4" />
       <Skeleton className="my-8 h-1 w-full" />
 
