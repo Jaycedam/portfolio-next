5daf31867931a1a83d965fb6d49db30500ae5f2c
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-01 16:08:15 -0300

    remove hero margin, moved to glogal section margin in css

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <section className="container my-16 flex flex-col items-center justify-center gap-4 text-center">
+    <section className="container flex flex-col items-center justify-center gap-4 text-center">
       {/* hero graphic  */}
       <div className="w-full max-w-md [&>*]:h-full [&>*]:w-full [&>*]:object-contain">
         <LaptopSVG />
