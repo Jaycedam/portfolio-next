b22e667ea86c478460322d4803939466ee6075d3
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-01 16:29:16 -0300

    added links to footer icons

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,23 +1,27 @@
-import { FaGithub, FaLinkedin, FaDribbble } from "react-icons/fa";
+import { FaGithub, FaDribbble } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <footer className="container flex flex-col items-center justify-between gap-8 py-16 text-muted-foreground md:flex-row">
-      <a href="#">
+      <a href={process.env.EMAIL}>
         <p>contact@jordancortes.dev</p>
       </a>
       <div className="flex gap-8">
-        <a href="#" className="flex items-center gap-2">
+        <a
+          href="https://github.com/Jaycedam"
+          className="flex items-center gap-2"
+          target="_blank"
+        >
           <FaGithub />
           <p>GitHub</p>
         </a>
-        <a href="#" className="flex items-center gap-2">
+        <a
+          href="https://dribbble.com/jaycedam"
+          className="flex items-center gap-2"
+          target="_blank"
+        >
           <FaDribbble />
           <p>Dribble</p>
-        </a>
-        <a href="#" className="flex items-center gap-2">
-          <FaLinkedin />
-          <p>LinkedIn</p>
         </a>
       </div>
     </footer>
