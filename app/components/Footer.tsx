bb87edcc347c1222a750074a469fe58c2f1f2d6b
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-01 01:31:33 -0300

    new footer content

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,17 +1,25 @@
-import { AiOutlineArrowUp } from "react-icons/ai";
-import { buttonVariants } from "@/components/ui/button";
+import { FaGithub, FaLinkedin, FaDribbble } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="container flex items-center justify-start py-16">
-      <a
-        aria-label="Back to top"
-        href="#top"
-        className={buttonVariants({ variant: "ghost" })}
-      >
-        <AiOutlineArrowUp className="h-[1.2rem] w-[1.2rem]" />
-        &nbsp;Volver al inicio
+    <footer className="container flex flex-col items-center justify-between gap-8 py-16 text-muted-foreground md:flex-row">
+      <a href="#">
+        <p>contact@jordancortes.dev</p>
       </a>
+      <div className="flex gap-8">
+        <a href="#" className="flex items-center gap-2">
+          <FaGithub />
+          <p>GitHub</p>
+        </a>
+        <a href="#" className="flex items-center gap-2">
+          <FaDribbble />
+          <p>Dribble</p>
+        </a>
+        <a href="#" className="flex items-center gap-2">
+          <FaLinkedin />
+          <p>LinkedIn</p>
+        </a>
+      </div>
     </footer>
   );
 }
