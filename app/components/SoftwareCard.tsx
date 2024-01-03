05adcb9b5504f2f392e462c9a637d5aab8c6b535
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-03 01:13:18 -0300

    separate software name and type in two lines

--- app/components/SoftwareCard.tsx
+++ app/components/SoftwareCard.tsx
@@ -5,19 +5,11 @@ import { ExtendedSoftware } from "@/utils/interfaces";
 export default function SoftwareCard(props: ExtendedSoftware) {
   return (
     <Link href={`/software/${props.id}`}>
-      <div
-        className="group relative isolate aspect-square overflow-hidden rounded-md
-             transition-all duration-500"
-      >
+      <div className="group relative isolate aspect-square overflow-hidden rounded-md transition-all duration-500">
         {/* overlay  */}
-        <div
-          className="pointer-events-none absolute bottom-0 left-0 z-20 grid
-              w-full items-center justify-center bg-gradient-to-t
-              from-zinc-950/80 pb-8 
-              pt-12 text-lg 
-              text-zinc-50"
-        >
-          {props.name} - {props.area.name}
+        <div className="pointer-events-none absolute bottom-0 left-0 z-20 grid w-full items-center justify-center bg-gradient-to-t from-zinc-950/90 px-2 pb-8 pt-12 text-center">
+          <p className="text-lg font-bold text-zinc-50">{props.name}</p>
+          <p className="text-sm font-light">{props.area.name}</p>
         </div>
         <Image
           src={props.imageUrl}
@@ -25,9 +17,7 @@ export default function SoftwareCard(props: ExtendedSoftware) {
           quality={100}
           fill
           sizes="(max-width: 768px) 100vw, 50vw"
-          className="h-full w-full object-cover 
-            transition-all duration-500 group-hover:scale-110 group-active:scale-100
-            "
+          className="h-full w-full object-cover transition-all duration-500 group-hover:scale-110 group-active:scale-100"
         />
       </div>
     </Link>
