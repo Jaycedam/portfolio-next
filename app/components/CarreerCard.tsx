cacb1f5eb47b1031007f38b66f0019dc9947990d
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-03 01:46:27 -0300

    change color to muted to carreer desc

--- app/components/CarreerCard.tsx
+++ app/components/CarreerCard.tsx
@@ -14,7 +14,7 @@ export default function CarreerCard(props: ExtendedCarreer) {
       key={props.id}
       className="grid w-full p-6 md:w-2/4 md:odd:place-self-start md:odd:text-end md:even:place-self-end"
     >
-      {/* due to localization mismatch on server/client 
+      {/* due to date localization mismatch on server/client 
       I added suppressHydrationWarning to prevent log this issue on browser */}
       <p className="text-sm font-light" suppressHydrationWarning>
         <strong className="font-normal uppercase text-pink-500">
@@ -36,7 +36,7 @@ export default function CarreerCard(props: ExtendedCarreer) {
         {props.name} - {props.company}
       </p>
 
-      <p className="mt-1">{props.about}</p>
+      <p className="mt-1 text-muted-foreground">{props.about}</p>
     </motion.div>
   );
 }
