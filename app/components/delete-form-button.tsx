53564ca5e228589a224ea43f90addf468229ccaa
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-08 17:33:13 -0300

    added confirmation on delete from table

--- app/components/delete-form-button.tsx
+++ app/components/delete-form-button.tsx
@@ -1,18 +1,43 @@
 import { DeleteProject } from "@/utils/actions";
 import React from "react";
 import { Button } from "@/components/ui/button";
-
-// add all delete actions from utils/actions
-// add interface to check what table is selected to delete entry
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 export default function DeleteFormButton(props: { id: number }) {
   // add check for selected option to delete based on interface
   return (
-    <form action={DeleteProject}>
-      <input type="hidden" readOnly name="id" defaultValue={props.id} />
-      <Button variant="destructive" type="submit">
-        Delete
-      </Button>
-    </form>
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Button variant="destructive">Delete</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>
+            <form action={DeleteProject}>
+              <input type="hidden" readOnly name="id" defaultValue={props.id} />
+              <button type="submit">Delete</button>
+            </form>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
