6512096d1c02c3339e9011d0b6b4133bc100ac4c
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-10 12:56:03 -0300

    fix for update in carreer type

--- app/(admin)/admin/carreer-type/update/[id]/page.tsx
+++ app/(admin)/admin/carreer-type/update/[id]/page.tsx
@@ -1,8 +1,11 @@
 import CarreerTypeForm from "@/components/form/carreer-type-form";
 import { getCarreerType } from "@/utils/get-data";
-import React from "react";
 
-export default async function page({ params }: { params: { id: string } }) {
+export default async function UpdateCarreerType({
+  params,
+}: {
+  params: { id: string };
+}) {
   const data = await getCarreerType(Number(params.id));
-  return <CarreerTypeForm />;
+  return <CarreerTypeForm carreerType={data} />;
 }
