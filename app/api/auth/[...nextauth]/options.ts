684398a0cd7fbd8613973a42a258bdb06a61e26d
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-12 03:25:31 -0300

    login form validation with zod

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -2,6 +2,7 @@ import { comparePasswords } from "@/lib/bcrypt";
 import { getUser } from "@/utils/get-data";
 import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { z } from "zod";
 
 export const options: NextAuthOptions = {
   providers: [
@@ -20,20 +21,26 @@ export const options: NextAuthOptions = {
       // next auth bug with strict mode, https://github.com/nextauthjs/next-auth/issues/2701
       // @ts-ignore
       async authorize(credentials) {
-        // get user from db and hash password
-        try {
-          const user = await getUser(credentials?.username);
-          // compares form password and hashed password from db
+        const parsedCredentials = z
+          .object({ username: z.string(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (parsedCredentials.success) {
+          const { username, password } = parsedCredentials.data;
+          const user = await getUser(username);
+
+          if (!user) return null;
+
           const passwordsMatch: boolean = await comparePasswords(
-            credentials?.password,
+            password,
             user.password
           );
+
           if (passwordsMatch) {
             return user;
           }
-          return null;
-        } catch (error) {
-          console.log(error);
+
+          console.log("Invalid credentials");
           return null;
         }
       },
