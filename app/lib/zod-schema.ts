40e79ee6410dfb545864944e2c2067865fc7bd94
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-12 18:06:50 -0300

    zod types

--- app/lib/zod-schema.ts
+++ app/lib/zod-schema.ts
@@ -7,10 +7,12 @@ export const projectSchema = z.object({
   homepage: z.boolean(),
   areaId: z.number().int(),
 });
+export type TProject = z.infer<typeof projectSchema>;
 
 export const projectAreaSchema = z.object({
   name: z.string().min(3),
 });
+export type TProjectArea = z.infer<typeof projectAreaSchema>;
 
 export const carreerSchema = z.object({
   name: z.string(),
@@ -19,12 +21,15 @@ export const carreerSchema = z.object({
   date: z.string(),
   typeId: z.number().int(),
 });
+export type TCarreer = z.infer<typeof carreerSchema>;
 
 export const carreerTypeSchema = z.object({
   name: z.string(),
 });
+export type TCarreerType = z.infer<typeof carreerTypeSchema>;
 
 export const userSchema = z.object({
   username: z.string().min(3),
   password: z.string().min(10),
 });
+export type TUser = z.infer<typeof userSchema>;
