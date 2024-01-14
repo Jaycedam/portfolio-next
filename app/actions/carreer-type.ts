0c19d706efad47b5a83a6295fafef767343fd967
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-14 20:59:44 -0300

    feat: admin form validation with zod and react-hook-form for carreer type

--- app/actions/carreer-type.ts
+++ app/actions/carreer-type.ts
@@ -1,53 +1,87 @@
 "use server";
 
 import prisma from "@/lib/prisma";
+import { TCarreerType, carreerTypeSchema } from "@/lib/zod-schema";
 import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
 
 const url: string = "/admin/carreer-type";
 
-export async function CreateCarreerType(formData: FormData) {
+export async function CreateCarreerType(data: TCarreerType) {
+  let errorMessage: string =
+    "Carreer Type could not be created, try again later.";
+  let successMessage: string = "Carreer Type created successfully.";
+
+  const parsedData = carreerTypeSchema.safeParse(data);
+
+  if (!parsedData.success) {
+    return {
+      error: errorMessage,
+    };
+  }
+
   try {
-    const update = await prisma.type.create({
-      data: {
-        name: formData.get("name") as string,
-      },
+    await prisma.type.create({
+      data: parsedData.data,
     });
-  } catch (error) {
-    console.log(error);
+    return {
+      success: successMessage,
+    };
+  } catch (e: any) {
+    return {
+      error: errorMessage + " " + e.message,
+    };
   } finally {
     revalidatePath(url);
-    redirect(url);
   }
 }
 
-export async function UpdateCarreerType(formData: FormData) {
+export async function UpdateCarreerType(data: TCarreerType) {
+  let errorMessage: string =
+    "Carreer Type could not be updated, try again later.";
+  let successMessage: string = "Carreer Type updated successfully.";
+
+  const parsedData = carreerTypeSchema.safeParse(data);
+
+  if (!parsedData.success) {
+    return {
+      error: errorMessage,
+    };
+  }
+
   try {
-    const update = await prisma.type.update({
+    await prisma.type.update({
       where: {
-        id: Number(formData.get("id")),
+        id: data.id,
       },
-      data: {
-        name: formData.get("name") as string,
-      },
+      data: parsedData.data,
     });
-  } catch (error) {
-    console.log(error);
+    return {
+      success: successMessage,
+    };
+  } catch (e: any) {
+    return {
+      error: errorMessage + " " + e.message,
+    };
   } finally {
     revalidatePath(url);
-    redirect(url);
   }
 }
 
 export async function DeleteCarreerType(formData: FormData) {
   try {
-    const update = await prisma.type.delete({
+    await prisma.type.delete({
       where: {
         id: Number(formData.get("id")),
       },
     });
-  } catch (error) {
-    console.log(error);
+
+    return {
+      success: "Project successfully deleted.",
+    };
+  } catch (e: any) {
+    return {
+      error: e.message,
+    };
   } finally {
     revalidatePath(url);
   }
--- app/components/form/carreer-type-form.tsx
+++ app/components/form/carreer-type-form.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { Type } from "@prisma/client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Area } from "@prisma/client";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -12,46 +13,95 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { carreerTypeSchema } from "@/lib/zod-schema";
+import { toast } from "sonner";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  // FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
 import { CreateCarreerType, UpdateCarreerType } from "@/actions/carreer-type";
 
-export default function CarreerTypeForm(props: { carreerType?: Type }) {
-  // check if object is being passed down to update, else create new one on db
-  const formAction = props.carreerType ? UpdateCarreerType : CreateCarreerType;
-  const formTitle = props.carreerType
-    ? "Update Carreer Type"
-    : "Create Carreer Type";
+export default function CarreerType({ carreerType }: { carreerType?: Area }) {
+  // check if project is being passed down to update, else create new one on db
+  const formAction = carreerType ? UpdateCarreerType : CreateCarreerType;
+  const formTitle = carreerType ? "Update Carreer Type" : "Create Carreer Type";
+
+  // form definition
+  const form = useForm<z.infer<typeof carreerTypeSchema>>({
+    resolver: zodResolver(carreerTypeSchema),
+    defaultValues: {
+      id: carreerType?.id,
+      name: carreerType?.name || "",
+    },
+  });
+
+  // form on submit
+  const handleSubmit = async (data: z.infer<typeof carreerTypeSchema>) => {
+    // server action to create or update
+    const result = await formAction(data);
+
+    // show toast of server returned result, reset form if successful
+    if (result?.success) {
+      toast.success(result.success);
+      if (carreerType === undefined) {
+        form.reset();
+      }
+    } else if (result?.error) {
+      toast.error(result.error);
+    }
+  };
 
   return (
-    <Card className="mx-auto max-w-lg">
+    <Card className="mx-auto max-w-xl">
       <CardHeader>
         <CardTitle>{formTitle}</CardTitle>
         <CardDescription>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero, ut.
         </CardDescription>
       </CardHeader>
-      <form action={formAction}>
-        <CardContent className="grid gap-6">
-          <Input
-            type="hidden"
-            readOnly
-            name="id"
-            defaultValue={props.carreerType?.id}
-          />
 
-          <div className="grid w-full  items-center gap-1.5">
-            <Label htmlFor="name">Name</Label>
-            <Input
-              required
-              type="text"
-              name="name"
-              defaultValue={props.carreerType?.name}
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)}>
+          <CardContent className="grid gap-3">
+            <FormField
+              control={form.control}
+              name="id"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input aria-hidden readOnly type="hidden" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
             />
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button type="submit">Save</Button>
-        </CardFooter>
-      </form>
+
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </CardContent>
+
+          <CardFooter>
+            <Button type="submit">Save</Button>
+          </CardFooter>
+        </form>
+      </Form>
     </Card>
   );
 }
