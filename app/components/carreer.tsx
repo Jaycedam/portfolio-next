1b72c43173fd05de178122762c3aebbc3fea0ca5
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-16 17:46:34 -0300

    style: move carreer line on mobile

--- app/components/carreer.tsx
+++ app/components/carreer.tsx
@@ -7,7 +7,7 @@ export default async function Carreer() {
   return (
     <section
       id="carreer"
-      className="relative mx-auto grid gap-4 from-transparent from-5% via-primary to-transparent after:absolute after:left-4 after:h-full after:w-1 after:bg-gradient-to-b md:w-3/4 md:after:left-2/4"
+      className="relative mx-auto grid gap-4 from-transparent from-5% via-primary to-transparent after:absolute after:left-0 after:h-full after:w-1 after:bg-gradient-to-b md:w-3/4 md:after:left-2/4"
     >
       <header>
         <h1 className="title text-center">Carrera Profesional</h1>
