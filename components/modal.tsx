982f06868e4e13f382322bbc557333281375f2cd
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-19 02:46:08 -0300

    style: fix border of modals, change hero text gradient

--- components/modal.tsx
+++ components/modal.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-
 import { Sheet, SheetContent } from "@/components/ui/sheet";
 
 export default function Modal({ children }: { children: React.ReactNode }) {
@@ -14,7 +13,7 @@ export default function Modal({ children }: { children: React.ReactNode }) {
     <Sheet open={true} onOpenChange={handleClose}>
       <SheetContent
         side="bottom"
-        className="h-[90%] overflow-auto rounded-2xl py-8 md:h-[95%]"
+        className="h-[93%] overflow-auto rounded-t-2xl py-8"
       >
         {children}
       </SheetContent>
