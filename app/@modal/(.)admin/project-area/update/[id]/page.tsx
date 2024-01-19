bf2ca8977386b89c56de541173143a94169d3943
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-18 21:10:26 -0300

    feat: update forms now appear as modal with Intercepting Routes

---
+++ app/@modal/(.)admin/project-area/update/[id]/page.tsx
@@ -0,0 +1,14 @@
+import { Suspense } from "react";
+import ModalDialog from "@/components/modal-dialog";
+import FormLoader from "@/components/form-loader";
+import SkeletonAdminTable from "@/components/skeleton/skeleton-admin-table";
+
+export default function Page({ params }: { params: { id: string } }) {
+  return (
+    <ModalDialog>
+      <Suspense fallback={<SkeletonAdminTable />}>
+        <FormLoader id={Number(params.id)} type="project-area" />
+      </Suspense>
+    </ModalDialog>
+  );
+}
