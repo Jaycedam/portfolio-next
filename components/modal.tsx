9958e848715e845d53028d01278d11d742fafd7d
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-19 00:53:38 -0300

    fix: change drawer component to sheet due to drag bug on Chrome mobile

--- components/modal.tsx
+++ components/modal.tsx
@@ -1,33 +1,21 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { Drawer, DrawerClose, DrawerContent } from "@/components/ui/drawer";
-import { Button } from "./ui/button";
-import { IoClose } from "react-icons/io5";
+
+import { Sheet, SheetContent } from "@/components/ui/sheet";
 
 export default function Modal({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const handleClose = () => router.back();
 
   return (
-    <Drawer shouldScaleBackground open={true} onClose={handleClose}>
-      <DrawerContent
-        onPointerDownOutside={handleClose}
-        onEscapeKeyDown={handleClose}
-        className="h-[90%] focus:outline-none md:h-[95%]"
+    <Sheet open={true} onOpenChange={handleClose}>
+      <SheetContent
+        side="bottom"
+        className="h-[90%] overflow-auto py-16 md:h-[95%]"
       >
-        <DrawerClose
-          className="absolute right-4 top-4 z-50 hidden md:block"
-          onClick={handleClose}
-          asChild
-        >
-          <Button className="h-6 w-6 rounded-full" size="icon">
-            <IoClose className="mx-auto h-6 w-auto" />
-          </Button>
-        </DrawerClose>
-
-        <div className="w-full overflow-auto px-4 py-8">{children}</div>
-      </DrawerContent>
-    </Drawer>
+        {children}
+      </SheetContent>
+    </Sheet>
   );
 }
