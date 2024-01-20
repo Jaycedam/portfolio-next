eb9a960192fc0287cbd28aff695a84e8116741fa
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-20 17:04:07 -0300

    chore: enable analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,8 +36,8 @@ export default function RootLayout({
             {children}
           </main>
           <Toaster />
-          {/* <SpeedInsights /> */}
-          {/* <Analytics /> */}
+          <SpeedInsights />
+          <Analytics />
           <Footer />
         </Providers>
       </body>
