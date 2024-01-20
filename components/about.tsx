32f02186e465a1ed1436c2daa9c6bcdf58482d00
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-20 20:33:43 -0300

    style: remove img from about section, delete carreer card component, redo carreer timeline

--- components/about.tsx
+++ components/about.tsx
@@ -3,44 +3,28 @@ import profile from "/public/profile.webp";
 
 export default function About() {
   return (
-    <section
-      id="about"
-      className="grid items-center gap-8 
-        md:grid-flow-col md:grid-cols-2"
-    >
+    <article id="about" className="mx-auto max-w-4xl text-foreground">
       {/* text section  */}
-      <div className="grid place-items-center text-center">
-        <header>
-          <h2 className="title">Acerca de mí</h2>
-        </header>
-        <p>
-          Hola, soy Jordan Cortés, Desarrollador de Software que disfruta
-          aprendiendo nuevas tecnologías. Titulado Analista Programador en Duoc
-          UC - 2022.
-          <br />
-          <br />
-          Previamente, mantuve una carrera trabajando en Motion Graphics por un
-          poco más de 4 años, después de finalizar Comunicación Audiovisual en
-          Santo Tomas - 2015.
-          <br />
-          <br />
-          Para ver mis proyectos destacados Motion Graphics, visitar{" "}
-          <a
-            href="https://dribbble.com/Jaycedam"
-            target="_blank"
-            className="font-bold text-primary"
-          >
-            Dribbble.
-          </a>
-        </p>
-      </div>
-
-      <Image
-        className="mx-auto aspect-square h-auto w-2/4 rounded-md object-cover md:w-full"
-        placeholder="blur"
-        src={profile}
-        alt=""
-      />
-    </section>
+      <header>
+        <h2 className="title">Acerca de mí</h2>
+      </header>
+      <p className="text-justify text-muted-foreground">
+        Hola, soy Jordan Cortés, Desarrollador de Software que disfruta
+        aprendiendo nuevas tecnologías. Titulado Analista Programador en Duoc UC
+        - 2022.
+        <br />
+        <br />
+        Previamente, mantuve una carrera trabajando en Motion Graphics por un
+        poco más de 4 años, después de finalizar Comunicación Audiovisual en
+        Santo Tomas - 2015. Para ver esos proyectos destacados visitar &nbsp;
+        <a
+          href="https://dribbble.com/Jaycedam"
+          target="_blank"
+          className="font-bold text-primary"
+        >
+          Dribbble.
+        </a>
+      </p>
+    </article>
   );
 }
--- components/carreer.tsx
+++ components/carreer.tsx
@@ -1,20 +1,49 @@
-import CarreerCard from "@components/carreer-card";
 import { getCarreers } from "@/actions/carreer";
+import { MdOutlineWorkOutline } from "react-icons/md";
+import { GiGraduateCap } from "react-icons/gi";
+import { MotionLi } from "./motion-elements";
+import { fade } from "@/utils/animations";
 
 export default async function Carreer() {
   const data = await getCarreers();
   return (
-    <section
-      id="carreer"
-      className="relative mx-auto grid gap-4 from-transparent from-5% via-primary to-transparent after:absolute after:left-0 after:h-full after:w-1 after:bg-gradient-to-b md:w-3/4 md:after:left-2/4"
-    >
+    <section id="carreer">
       <header>
         <h1 className="title text-center">Carrera Profesional</h1>
       </header>
 
-      {data.map((item) => (
-        <CarreerCard key={item.id} {...item} />
-      ))}
+      <ol className="prose relative mx-auto border-s border-border">
+        {data.map((item, idx) => (
+          <MotionLi
+            variants={fade}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ amount: 0.5 }}
+            key={idx}
+            className="mb-10 ms-6"
+          >
+            <span className="absolute -start-3 flex h-8 w-8 items-center justify-center rounded-full bg-primary text-background ">
+              {item.type.id === 1 ? (
+                <MdOutlineWorkOutline className="h-6 w-6" />
+              ) : (
+                <GiGraduateCap className="h-6 w-6" />
+              )}
+            </span>
+            <h3 className="mb-1 flex items-center text-lg font-semibold text-foreground">
+              {item.name} - {item.company}
+              <span className="me-2 ms-3 rounded bg-muted px-2.5 py-0.5 text-sm font-medium">
+                {item.type.name}
+              </span>
+            </h3>
+            <time className="mb-2 block text-sm font-normal leading-none text-muted-foreground">
+              {item.date}
+            </time>
+            <p className="mb-4 text-base font-normal text-muted-foreground">
+              {item.about}
+            </p>
+          </MotionLi>
+        ))}
+      </ol>
     </section>
   );
 }
