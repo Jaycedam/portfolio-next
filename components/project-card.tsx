e35aac12b6bab17875e63260d5441c7c8d037db0
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-21 19:44:44 -0300

    style: change colors card

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -5,11 +5,11 @@ import { ExtendedProject } from "@utils/interfaces";
 export default function ProjectCard(props: ExtendedProject) {
   return (
     <Link href={`/projects/${props.id}`}>
-      <div className="group relative isolate aspect-square overflow-hidden rounded-2xl transition-all duration-500">
+      <div className="group relative isolate aspect-square overflow-hidden rounded-2xl border transition-all duration-500">
         {/* overlay  */}
-        <div className="pointer-events-none absolute bottom-0 left-0 z-20 grid w-full items-center justify-center bg-gradient-to-t from-zinc-950/90 px-2 pb-8 pt-12 text-center text-zinc-50">
-          <p className="text-xl font-bold">{props.name}</p>
-          <p className="text-sm font-light">{props.area.name}</p>
+        <div className="pointer-events-none absolute bottom-0 z-10 flex h-[20%] w-full flex-col items-center justify-center bg-gradient-to-t from-black px-2 text-center text-zinc-50 transition-all">
+          <p className="text-2xl font-bold">{props.name}</p>
+          <p className="text-sm text-muted-foreground">{props.area.name}</p>
         </div>
         <Image
           src={props.imageUrl}
@@ -20,7 +20,7 @@ export default function ProjectCard(props: ExtendedProject) {
           iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNMUQQAAO8Ah7R22bwAAAAASUVORK5CYII="
           fill
           sizes="(max-width: 768px) 100vw, 50vw"
-          className="h-[95%] w-[95%] object-cover transition-all duration-500 group-hover:scale-110 group-active:scale-100"
+          className="object-cover transition-all duration-500 group-hover:scale-110 group-active:scale-100"
         />
       </div>
     </Link>
