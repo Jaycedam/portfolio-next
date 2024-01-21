02eb0de0c7f63d0df4265730c8ae5d66cfe92175
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-20 22:43:42 -0300

    style: toast with rich colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
             {modal}
             {children}
           </main>
-          <Toaster />
+          <Toaster richColors />
           <SpeedInsights />
           <Analytics />
           <Footer />
