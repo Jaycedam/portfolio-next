b54bd8209b8034be54023aef9cc683ed6495b550
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-21 21:54:36 -0300

    style: change article styles on mdx

--- app/@modal/(.)projects/[id]/page.tsx
+++ app/@modal/(.)projects/[id]/page.tsx
@@ -8,13 +8,7 @@ export default function Page({ params }: { params: { id: string } }) {
 
   return (
     <Modal>
-      <article
-        className="prose mx-auto max-w-4xl py-16 dark:prose-invert
-            prose-a:mr-4
-            prose-em:text-sm prose-em:text-muted-foreground 
-            prose-img:aspect-[4/3] prose-img:w-full prose-img:rounded-md prose-img:border prose-img:object-fill
-            prose-video:aspect-[16/10] prose-video:w-full prose-video:rounded-md prose-video:border"
-      >
+      <article className="prose mx-auto max-w-4xl py-16 dark:prose-invert prose-em:text-sm prose-em:text-muted-foreground prose-img:aspect-video prose-img:w-full prose-img:rounded-2xl prose-img:border prose-img:object-cover prose-video:aspect-video prose-video:w-full prose-video:rounded-md prose-video:border">
         <Suspense fallback={<SkeletonArticle />}>
           <MDX id={id} />
         </Suspense>
