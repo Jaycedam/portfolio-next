0fd55cc73165c0f8acc310c5173543fcf79c3059
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-27 20:32:01 -0300

    fix: when mdxremote fails to compile the file, it's handled and returns 404 instead of crashing

--- components/mdx-remote.tsx
+++ components/mdx-remote.tsx
@@ -2,35 +2,43 @@ import { getProjectByName } from "@/actions/project";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import { notFound } from "next/navigation";
 import { HeaderImage, LinkButton } from "@components/mdx-components";
+import { MDXProvider } from "@mdx-js/react";
 
 export default async function MDX({ name }: { name: string }) {
-  try {
-    const project = await getProjectByName(name);
+  const project = await getProjectByName(name);
 
-    const parsedUrl = new URL(project.url);
-    const res = await fetch(parsedUrl.toString(), {
-      // revalidate cache in 1 hour
-      next: { revalidate: 3600 },
-      // enable no cache on dev mode
-      // next: { revalidate: 0 },
-    });
+  const parsedUrl = new URL(project.url);
+  const res = await fetch(parsedUrl.toString(), {
+    // revalidate cache in 1 hour
+    next: { revalidate: 3600 },
+    // enable no cache on dev mode
+    // next: { revalidate: 0 },
+  });
 
-    if (!res.ok) {
-      throw new Error(`Failed to fetch content from ${parsedUrl}`);
-    }
+  const markdown = await res.text();
 
-    const markdown = await res.text();
+  // custom components for MDX
+  const components = { HeaderImage, LinkButton };
 
-    // custom components for MDX
-    const components = { HeaderImage, LinkButton };
-
-    return (
-      <article className="prose prose-zinc mx-auto dark:prose-invert prose-a:text-primary prose-em:text-sm prose-em:text-muted-foreground prose-hr:border-border dark:prose-pre:bg-muted">
-        <MDXRemote source={markdown} components={components} />
-      </article>
-    );
-  } catch (error) {
-    console.error("Error fetching or rendering MDX:", error);
-    return notFound();
-  }
+  return (
+    <article className="prose prose-zinc mx-auto dark:prose-invert prose-a:text-primary prose-em:text-sm prose-em:text-muted-foreground prose-hr:border-border dark:prose-pre:bg-muted">
+      <CompiledMDX source={markdown} components={components} />
+    </article>
+  );
 }
+
+const CompiledMDX = async ({
+  source,
+  components,
+}: {
+  source: string;
+  components?: React.ComponentProps<typeof MDXProvider>["components"];
+}) => {
+  try {
+    // it has to return with await since it returns a promise
+    return await MDXRemote({ source: source, components: components });
+  } catch (error) {
+    // handle the error here
+    notFound();
+  }
+};
