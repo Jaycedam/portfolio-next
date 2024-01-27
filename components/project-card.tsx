3a4fce7fd9a819cff0974497820d329bb1bd7718
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-27 12:56:35 -0300

    style: change overlay of project card to a min-h instead of h to prevent text overlap

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -12,7 +12,7 @@ export default function ProjectCard({ name, area, imageUrl }: ExtendedProject) {
     <Link href={`/projects/${slug}`} scroll={false}>
       <div className="group relative isolate aspect-square overflow-hidden rounded-xl border transition-all duration-500">
         {/* overlay  */}
-        <div className="pointer-events-none absolute bottom-0 z-10 flex h-[20%] w-full flex-col items-center justify-center bg-gradient-to-t from-black/80 px-2 text-center text-zinc-50 transition-all">
+        <div className="pointer-events-none absolute bottom-0 z-10 flex min-h-[20%] w-full flex-col items-center justify-center bg-gradient-to-t from-black/80 p-4 text-center text-zinc-50 transition-all">
           <h2 className="text-2xl font-bold">{name}</h2>
           <p className="text-sm text-zinc-400">{area.name}</p>
         </div>
