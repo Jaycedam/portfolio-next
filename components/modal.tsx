e5e238501639b41d2995a20cfb35cd51614c1a1a
Jordan C <45575946+Jaycedam@users.noreply.github.com>
2024-01-28 22:57:03 -0300

    fix: closing modal animation now working instead of dismounting

--- components/modal.tsx
+++ components/modal.tsx
@@ -2,16 +2,29 @@
 
 import { useRouter } from "next/navigation";
 import { Sheet, SheetContent } from "@/components/ui/sheet";
+import { useState } from "react";
 
 export default function Modal({ children }: { children: React.ReactNode }) {
+  const [open, setOpen] = useState(true);
+
   const router = useRouter();
+
   const handleClose = () => {
-    router.back();
+    setOpen(false);
+  };
+
+  const handleAnimationEnd = () => {
+    // when the modal animation ends:
+    // if it's closed, then navigate to previous url history
+    if (!open) {
+      router.back();
+    }
   };
 
   return (
-    <Sheet open={true} onOpenChange={handleClose}>
+    <Sheet open={open} onOpenChange={handleClose}>
       <SheetContent
+        onAnimationEndCapture={handleAnimationEnd}
         side="bottom"
         className="h-full overflow-auto rounded-t-xl py-8 md:h-[95%]"
       >
