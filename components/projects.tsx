3c570923ca78ed7aa0f638d1f4f5dd3786c55316
Jay <45575946+Jaycedam@users.noreply.github.com>
2024-01-30 17:58:14 -0300

    refactor: move project card to projects component since it's not reusable

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,8 +1,9 @@
-import ProjectCard from "@components/project-card";
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
 import { ChevronRight, X } from "lucide-react";
 import { getMDXMeta } from "@/utils/fetch-mdx";
+import { MDXMeta } from "@/utils/types";
+import Image from "next/image";
 
 export default async function Projects({
   homepage = false,
@@ -78,5 +79,31 @@ export default async function Projects({
         )}
       </div>
     </section>
+  );
+}
+
+function ProjectCard({ id, image, title, area }: MDXMeta) {
+  return (
+    // scroll false to avoid scrolling to the top on modal
+    <Link href={id} scroll={false}>
+      <div className="group relative isolate aspect-square overflow-hidden rounded-xl border transition-all duration-500">
+        {/* overlay  */}
+        <div className="pointer-events-none absolute bottom-0 z-10 flex min-h-[20%] w-full flex-col items-center justify-center bg-gradient-to-t from-black/80 p-4 text-center text-zinc-50 transition-all">
+          <h2 className="text-xl font-bold">{title}</h2>
+          <p className="text-sm">{area}</p>
+        </div>
+        <Image
+          src={image}
+          alt="project-image"
+          quality={100}
+          placeholder="blur"
+          blurDataURL="data:image/png;base64,
+          iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNMUQQAAO8Ah7R22bwAAAAASUVORK5CYII="
+          fill
+          sizes="(max-width: 768px) 100vw, 50vw"
+          className="object-cover transition-all duration-500 group-hover:scale-110 group-active:scale-100"
+        />
+      </div>
+    </Link>
   );
 }
