76fdf0e9046a89cc8568a351ad8429ec920fce56
Jay <45575946+Jaycedam@users.noreply.github.com>
2024-01-31 17:44:46 -0300

    style: rm tailwind custom screen container, rm container div from footer and nav, taller modal

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -30,110 +30,108 @@ export default async function Navbar() {
   ];
 
   return (
-    <header className="sticky top-0 z-50 border-b border-border/40 bg-background/60 backdrop-blur">
-      <div className="container flex h-14 w-full items-center justify-between">
-        <div className="flex gap-4">
-          {/* logo  */}
-          <Link aria-label="homepage" href="/">
-            <LogoSVG size={10} />
-          </Link>
-          <nav className="hidden md:block">
-            <ul className="flex items-center gap-4">
+    <header className="sticky top-0 z-50 flex h-14 w-full items-center justify-between border-b border-border/40 bg-background/60 px-4 backdrop-blur md:px-8">
+      <div className="flex gap-4">
+        {/* logo  */}
+        <Link aria-label="homepage" href="/">
+          <LogoSVG size={10} />
+        </Link>
+        <nav className="hidden md:block">
+          <ul className="flex items-center gap-4">
+            {navLinks.map((item, index) => (
+              <li key={index}>
+                <NavLink href={item.href}>{item.label}</NavLink>
+              </li>
+            ))}
+
+            {session && (
+              <li>
+                <NavLink href="/admin">Admin</NavLink>
+              </li>
+            )}
+          </ul>
+        </nav>
+      </div>
+
+      {/* icons nav  */}
+      <nav className="hidden md:block">
+        <ul className="flex gap-2">
+          {session && (
+            <li>
+              <SignOutBtn />
+            </li>
+          )}
+
+          <li>
+            <ThemeToggle />
+          </li>
+          <li>
+            <Link
+              className={buttonVariants({ variant: "ghost", size: "icon" })}
+              href="/#contact"
+            >
+              <MailPlus className="h-5" />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      {/* mobile nav */}
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <Menu />
+          </SheetTrigger>
+          <SheetContent
+            side="top"
+            className="grid items-center justify-center rounded-b-xl text-center"
+          >
+            <ul className="flex flex-col flex-wrap items-center gap-4 px-8 py-16">
               {navLinks.map((item, index) => (
                 <li key={index}>
-                  <NavLink href={item.href}>{item.label}</NavLink>
+                  {/* SheetClose is used as child to close the nav when the child is clicked */}
+                  <SheetClose asChild>
+                    <NavLink href={item.href}>{item.label}</NavLink>
+                  </SheetClose>
                 </li>
               ))}
-
               {session && (
                 <li>
-                  <NavLink href="/admin">Admin</NavLink>
+                  <SheetClose asChild>
+                    <NavLink href="/admin">Admin</NavLink>
+                  </SheetClose>
                 </li>
               )}
             </ul>
-          </nav>
-        </div>
 
-        {/* icons nav  */}
-        <nav className="hidden md:block">
-          <ul className="flex gap-2">
-            {session && (
-              <li>
-                <SignOutBtn />
-              </li>
-            )}
-
-            <li>
-              <ThemeToggle />
-            </li>
-            <li>
-              <Link
-                className={buttonVariants({ variant: "ghost", size: "icon" })}
-                href="/#contact"
-              >
-                <MailPlus className="h-5" />
-              </Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* mobile nav */}
-        <div className="md:hidden">
-          <Sheet>
-            <SheetTrigger>
-              <Menu />
-            </SheetTrigger>
-            <SheetContent
-              side="top"
-              className="grid items-center justify-center rounded-b-xl text-center"
-            >
-              <ul className="flex flex-col flex-wrap items-center gap-4 px-8 py-16">
-                {navLinks.map((item, index) => (
-                  <li key={index}>
-                    {/* SheetClose is used as child to close the nav when the child is clicked */}
-                    <SheetClose asChild>
-                      <NavLink href={item.href}>{item.label}</NavLink>
-                    </SheetClose>
-                  </li>
-                ))}
-                {session && (
-                  <li>
-                    <SheetClose asChild>
-                      <NavLink href="/admin">Admin</NavLink>
-                    </SheetClose>
-                  </li>
-                )}
-              </ul>
-
-              <ul className="flex items-center justify-center gap-4">
-                {session && (
-                  <li>
-                    <SheetClose asChild>
-                      <SignOutBtn />
-                    </SheetClose>
-                  </li>
-                )}
-
-                <li>
-                  <ThemeToggle />
-                </li>
+            <ul className="flex items-center justify-center gap-4">
+              {session && (
                 <li>
                   <SheetClose asChild>
-                    <Link
-                      className={buttonVariants({
-                        variant: "ghost",
-                        size: "icon",
-                      })}
-                      href={"/#contact"}
-                    >
-                      <MailPlus className="h-5" />
-                    </Link>
+                    <SignOutBtn />
                   </SheetClose>
                 </li>
-              </ul>
-            </SheetContent>
-          </Sheet>
-        </div>
+              )}
+
+              <li>
+                <ThemeToggle />
+              </li>
+              <li>
+                <SheetClose asChild>
+                  <Link
+                    className={buttonVariants({
+                      variant: "ghost",
+                      size: "icon",
+                    })}
+                    href={"/#contact"}
+                  >
+                    <MailPlus className="h-5" />
+                  </Link>
+                </SheetClose>
+              </li>
+            </ul>
+          </SheetContent>
+        </Sheet>
       </div>
     </header>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,6 @@ const config: Config = {
     container: {
       center: true,
       padding: "1rem",
-      screens: {
-        "2xl": "1400px",
-      },
     },
     extend: {
       colors: {
