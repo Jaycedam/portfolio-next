573b39b4ddf0f86de85e149c917df000a1ecd1d0
Jay <45575946+Jaycedam@users.noreply.github.com>
2024-01-31 00:47:14 -0300

    chore: disable tag links until there's a fix for intercepting modal with link redirects

--- components/mdx-content.tsx
+++ components/mdx-content.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 import { Badge } from "./ui/badge";
-import Link from "next/link";
-import { buttonVariants } from "./ui/button";
 import { getMDXByName } from "@/utils/fetch-mdx";
 import { notFound } from "next/navigation";
 import { RepoFolder } from "@/utils/types";
@@ -26,32 +24,26 @@ const MDXContent = async ({
 
         <div className="mx-auto flex max-w-prose flex-wrap gap-2">
           {meta.tags.map((tag, idx) => (
-            <Link
-              className={`text-base font-normal ${buttonVariants({
-                variant: "outline",
-                size: "sm",
-              })}`}
-              key={idx}
-              href={`/${type}?tags=${tag}`}
-            >
+            <Badge key={idx} variant="outline">
               {tag}
-            </Link>
+            </Badge>
           ))}
         </div>
 
         <p className="prose text-muted-foreground">{meta.description}</p>
-      </div>
-      <div className="w-fulll relative aspect-video">
-        <Image
-          priority
-          placeholder="blur"
-          blurDataURL="data:image/png;base64,
+
+        <div className="w-fulll relative aspect-video">
+          <Image
+            priority
+            placeholder="blur"
+            blurDataURL="data:image/png;base64,
             iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNMUQQAAO8Ah7R22bwAAAAASUVORK5CYII="
-          src={meta.image}
-          alt="preview"
-          fill
-          className="rounded-xl object-cover"
-        />
+            src={meta.image}
+            alt="preview"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
       </div>
 
       <article className="prose prose-zinc mx-auto dark:prose-invert prose-a:text-primary prose-em:text-sm prose-em:text-muted-foreground prose-hr:border-border dark:prose-pre:bg-muted">
