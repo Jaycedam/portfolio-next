de905da33d16ca407f5d2e8b826e96aec813f34d
Jay <45575946+Jaycedam@users.noreply.github.com>
2024-02-04 13:19:23 -0300

    feat: don't show language on url

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import { NextRequest } from "next/server";
 const I18nMiddleware = createI18nMiddleware({
   locales: ["en", "es"],
   defaultLocale: "en",
+  urlMappingStrategy: "rewrite",
 });
 
 export function middleware(request: NextRequest) {
