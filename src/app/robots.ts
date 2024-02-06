fa19d75e6ea805715ce013a72ff035bd86947e1d
Jay <45575946+Jaycedam@users.noreply.github.com>
2024-02-06 15:22:35 -0300

    chore: missing robot.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next";
+
+// Documentation https://nextjs.org/docs/app/api-reference/file-conventions/metadata/robots
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/admin/",
+    },
+    sitemap: process.env.SITE_URL,
+  };
+}
