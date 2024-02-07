8e434a0816fbf7995d83435c36388cb435ab66d5
Jay <45575946+Jaycedam@users.noreply.github.com>
2024-02-07 15:03:03 -0300

    style: update article skeleton

--- src/components/skeleton/skeleton-article.tsx
+++ src/components/skeleton/skeleton-article.tsx
@@ -2,19 +2,23 @@ import { Skeleton } from "@components/ui/skeleton";
 
 export default function SkeletonArticle() {
   return (
-    <div className="mx-auto min-h-screen max-w-prose space-y-8">
-      <Skeleton className="h-12 w-2/4" />
-      <Skeleton className="h-4 w-3/4" />
-
-      <div className="mt-4 space-y-4">
-        <Skeleton className="aspect-video w-full" />
-
-        <Skeleton className="h-4 w-full" />
+    <div className="container min-h-screen space-y-8">
+      <div className="mx-auto max-w-prose space-y-4">
+        <Skeleton className="h-4 w-16" />
+        <Skeleton className="h-12 w-2/4" />
         <Skeleton className="h-4 w-full" />
         <Skeleton className="h-4 w-full" />
       </div>
-      <div className="mt-4 space-y-4">
-        <Skeleton className="h-8 w-2/4" />
+
+      <Skeleton className="aspect-video w-full rounded-xl" />
+
+      <div className="mx-auto max-w-prose space-y-4">
+        <Skeleton className="h-12 w-2/4" />
+
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
         <Skeleton className="h-4 w-full" />
         <Skeleton className="h-4 w-full" />
       </div>
