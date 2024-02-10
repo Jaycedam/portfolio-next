100ca48245f1ab6bf2a2ead6e7809ce94c5159d7
Jay <45575946+Jaycedam@users.noreply.github.com>
2024-02-10 03:15:35 -0300

    style: change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@ import Footer from "@components/footer";
 import Navbar from "@components/navbar";
 import "@app/globals.css";
 import { Providers } from "@components/providers";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
+import { Inter, JetBrains_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Toaster } from "@components/ui/sonner";
 
@@ -11,6 +10,18 @@ export const metadata = {
   title: "Jordan Cortés",
   description: "Software Developer Portfolio",
 };
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
+
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-jetbrains-mono",
+});
 
 export default function RootLayout({
   children,
@@ -24,7 +35,7 @@ export default function RootLayout({
     // it's necessary for the next-themes pckg since it updates that element
     <html
       lang="es"
-      className={`${GeistSans.variable} ${GeistMono.variable} scroll-smooth`}
+      className={`${inter.variable} ${jetbrainsMono.variable} scroll-smooth`}
       suppressHydrationWarning
     >
       <body>
