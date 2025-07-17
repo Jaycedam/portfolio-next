5ecf940bbbd81fa05fa248bbee057af5b74af6f8
Jaycedam <45575946+Jaycedam@users.noreply.github.com>
2025-07-17 15:07:15 -0400

    fix: attempt to fix old version of react used by pkg

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "github.com",
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "github.com",
+            },
+        ],
+    },
+    transpilePackages: ["next-mdx-remote"],
 };
 
-module.exports = nextConfig;
+export default nextConfig;
